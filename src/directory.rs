2338763068a63ad43cd48623748fb252ab00596c
Daan de Graaf <daandegraaf9@gmail.com>
2016-01-30 17:26:07 +0100

    Redone

---
+++ src/directory.rs
@@ -0,0 +1,63 @@
+use std::path::{PathBuf, Path};
+use std::io::Error;
+use std::fs::read_dir;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct Directory {
+    pub path: PathBuf,
+    pub name: String
+}
+
+impl fmt::Display for Directory {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "name: {}, path: {}", self.name, self.path.to_str().unwrap())
+    }
+}
+
+impl Directory {
+    pub fn path_str<'a>(&'a self) -> &'a str{
+        self.path.to_str().unwrap()
+    }
+}
+
+pub type DirIter = Box<Iterator<Item = Directory>>;
+
+pub fn find_in(query: &Path, origin: &Path) -> Result<DirIter, Error> {
+    let mut base_dir = PathBuf::from(origin);
+    // We have to adjust the base directory if needed
+    let mut components = query.components();
+    let num_comp = query.components().count();
+
+    if num_comp > 0 {
+        for comp in components.by_ref().take(num_comp-1) {
+            let string = comp.as_os_str().to_str().expect("Invalid character in os str");
+            base_dir.push(string);
+        }
+    }
+
+    let query = match components.next() {
+        Some(comp) => comp.as_os_str().to_str().unwrap(),
+        None => ""
+    };
+
+    let query = String::from(query);
+
+    Ok(
+        Box::new(
+            try!(read_dir(base_dir)).filter_map(move |dir|{
+                let dir = dir.unwrap();
+                let dir_name = String::from(dir.file_name().to_str().unwrap());
+                if dir.file_type().unwrap().is_dir() && dir_name.starts_with(&query) {
+                    Some(Directory {
+                        path: PathBuf::from(dir.path()),
+                        name: dir_name
+                    })
+                }
+                else {
+                    None
+                }
+            })
+        )
+    )
+}
--- src/main.rs
+++ src/main.rs
@@ -1,112 +1,39 @@
 use std::env;
-use std::fs;
-use std::path::{PathBuf};
-use std::io::Error;
+use std::path::Path;
 
-const WORKSPACE_PATH: &'static str = "/home/daan/workspace/";
+mod directory;
+use directory::{find_in, DirIter};
 
-fn main() {
+fn main(){
     let mut args = env::args();
-    args.next();    //Skip first argument
-    let mode = args.next();
-    match mode {
-        Some(mode) => {
-            match args.next() {
-                Some(query) => {
-                    let query_path = PathBuf::from(&query);
-                    let base_iter = path_match(&query, true).unwrap_or(Box::new(Vec::new().into_iter()));
-                    
-                    let mut total_iter: Box<Iterator<Item=String>> = match query_path.has_root() {
-                        true => Box::new(base_iter),
-                        false => {
-                            let mut workspace_path = PathBuf::from(WORKSPACE_PATH);
-                            workspace_path.push(&query_path);
-                            let workspace_iter = path_match(workspace_path.to_str().unwrap(), mode == "go").unwrap_or(
-                                    Box::new(Vec::new().into_iter())
-                                ).chain(base_iter);
-                            Box::new(workspace_iter)
-                        },
-                        
-                    };
-                    
-                    if mode == "go" {
-                        match total_iter.next() {
-                            Some(string) => return println!("{}", string),
-                            None => return    
-                        }   
-                    }
-                    for string in total_iter {
-                        println!("{}", string);    
-                    }
-                },
-                None => {
-                    if mode != "list" { return }
-                    for string in path_match(WORKSPACE_PATH, false).unwrap() {
-                        println!("{}", string);    
-                    }
-                }   
-            }
-        },
-        None => return println!("No mode")    
+    args.next();    // Skip the first argument (the command path)
+
+    let mode = match args.next() {
+        Some(mode) => mode,
+        None => return println!("Mode not specified!")
+    };
+    let query = args.next().unwrap_or_default();
+    let query = Path::new(&query);
+
+    let base_path = Path::new("/home/daan/workspace");
+    let results = find_in(query, base_path).unwrap();
+
+    match mode.as_ref() {
+        "list" => list(results),
+        "go" => go(results),
+        _ => println!("Invalid mode!")
     }
-    
 }
 
-fn path_match(query_path: &str, full_path: bool) -> Result<Box<Iterator<Item=String>>, Error> {
-    let mut query_buf = PathBuf::from(query_path);
-    let clone = query_buf.clone();
-    
-    let query = match clone.file_name() {
-          Some(file_name) => {
-              //Handle queries that are relative but are not prefixed with ./
-              if query_buf.components().count() == 1 {
-                      query_buf = PathBuf::from("./");
-                      String::from(query_path)
-              }
-              //Handle queries that end in /
-              else if query_path.ends_with("/") { 
-                  String::from("")
-              }
-              else {
-                  query_buf.pop(); //Pop filename
-                  String::from(file_name.to_str().unwrap_or(""))
-              }
-          },
-          None => {
-              String::from("")    
-          }
-    };
-    
-    let mut base = Vec::new();
-    if query_buf.exists() && query.is_empty() {
-        base.push(String::from(query_buf.to_str().unwrap()));
+fn list(iter: DirIter) {
+    for item in iter {
+        println!("{}", item.name);
     }
-    
-    Ok(
-        Box::new(
-            base.into_iter().chain(
-                try!(fs::read_dir(query_buf.as_path())).filter_map(move |dir|{
-            
-                    let dir = dir.unwrap();
-                    let mut dir_name = String::from(dir.file_name().to_str().unwrap());
-                    if dir_name.starts_with(&query) && dir.file_type().unwrap().is_dir() {
-                        match full_path {
-                            true => {
-                                let mut full_path = String::from(dir.path().to_str().unwrap());
-                                full_path.push('/');
-                                Some(full_path)
-                            },
-                            false => {
-                                dir_name.push('/');
-                                Some(dir_name)
-                            }
-                        }    
-                    }
-                    else { 
-                        None   
-                    }
-                })
-            )
-        )
-    )
+}
+
+fn go(mut iter: DirIter) {
+    match iter.next() {
+        Some(dir) => println!("{}", dir.path_str()),
+        None => println!("No results found!")
+    }
 }
